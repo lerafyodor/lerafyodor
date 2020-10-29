dce9617fdd114e5268954d9ae89659592b870710
fegu0214 <fedor.guryanov@netcracker.com>
2020-10-29 19:39:34 +0300

    Initial commit

---
+++ precache-manifest.eeab65cc70cc335a67b3728cafeba13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fc4182a4718ecf05349eea0b3de257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22ee1a71bec3bf0f7aa",
+    "url": "/static/css/main.42cc3383.chunk.css"
+  },
+  {
+    "revision": "511b63466f9e1af72c4a",
+    "url": "/static/js/2.d2142cae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2142cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22ee1a71bec3bf0f7aa",
+    "url": "/static/js/main.a068adba.chunk.js"
+  },
+  {
+    "revision": "8b49eb0210d9e38fe863",
+    "url": "/static/js/runtime-main.aa5ab8f3.js"
+  }
+]);
